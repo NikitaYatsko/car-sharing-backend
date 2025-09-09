2505f36d70108a1ff3493b0511cf6a9308dfeefd
Nick <archon04@mail.ru>
2025-09-09 20:52:49 +0300

    first commmit

---
+++ src/main/java/com/example/car_sharing_backend/CarSharingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.car_sharing_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSharingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSharingBackendApplication.class, args);
+	}
+
+}
