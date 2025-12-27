7646cf5816d06f814e452c548e06fd781c4c2a81
NikitaYatsko <archon04@mail.ru>
2025-12-27 21:16:29 +0200

    n+1 resolved by using query method

--- src/main/java/com/example/car_sharing_backend/repository/BookingRepository.java
+++ src/main/java/com/example/car_sharing_backend/repository/BookingRepository.java
@@ -4,10 +4,12 @@ import com.example.car_sharing_backend.model.entity.Booking;
 
 import com.example.car_sharing_backend.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface BookingRepository extends JpaRepository<Booking, UUID> {
+    @Query("SELECT b FROM Booking b JOIN FETCH b.car WHERE b.user = :user")
     List<Booking> findByUser(User user);
 }
