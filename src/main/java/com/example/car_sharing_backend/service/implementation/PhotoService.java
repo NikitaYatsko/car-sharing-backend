fe3cad4334f4b2c0a3c819228ef1b95e6ac053ba
NikitaYatsko <archon04@mail.ru>
2026-01-05 21:31:55 +0200

    I created other services to ease main one of Driving License

--- src/main/java/com/example/car_sharing_backend/service/implementation/PhotoService.java
+++ src/main/java/com/example/car_sharing_backend/service/implementation/PhotoService.java
@@ -2,7 +2,8 @@ package com.example.car_sharing_backend.service.implementation;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
-import lombok.AllArgsConstructor;
+
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,14 +11,22 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.Map;
 
-@AllArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class PhotoService {
-    private Cloudinary cloudinary;
 
-    public String uploadPhoto(MultipartFile file) throws IOException {
-        Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
-        return (String) uploadResult.get("url");
+    private final Cloudinary cloudinary;
+
+    public String uploadPhoto(MultipartFile file, String folder) throws IOException {
+
+        if (file == null || file.isEmpty())
+            throw new IllegalArgumentException("File is empty");
+
+        Map<String, Object> result = cloudinary.uploader().upload(
+                file.getBytes(),
+                ObjectUtils.asMap("folder", folder)
+        );
+
+        return result.get("secure_url").toString();
     }
-
 }
