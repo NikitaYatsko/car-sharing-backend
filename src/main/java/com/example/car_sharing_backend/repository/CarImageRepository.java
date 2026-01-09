33a48e89f7106fdd18a93bdc08e3645e1e43f8e9
NikitaYatsko <archon04@mail.ru>
2026-01-09 16:11:56 +0200

    added entity graph to resolve n+1 problem, removed unused repository

--- src/main/java/com/example/car_sharing_backend/repository/CarImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.car_sharing_backend.repository;
-
-import com.example.car_sharing_backend.model.entity.CarImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CarImageRepository extends JpaRepository<CarImage, UUID> {
-}
--- src/main/java/com/example/car_sharing_backend/repository/CarRepository.java
+++ src/main/java/com/example/car_sharing_backend/repository/CarRepository.java
@@ -1,10 +1,17 @@
 package com.example.car_sharing_backend.repository;
 
 import com.example.car_sharing_backend.model.entity.Car;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CarRepository extends JpaRepository<Car, UUID> {
     boolean existsByStateNumber(String stateNumber);
+
+    @Override
+    @EntityGraph(attributePaths = "images")
+    List<Car> findAll();
 }
